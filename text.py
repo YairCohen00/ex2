2c941b89ee26755bbf5a731c911eb9b7708a6408
YairCohen00 <yac03979@gmail.com>
2021-03-20 16:30:18 +0200

    update2

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-file = open("C:\yair\course\ex2/text.txt")
+file = open("text.txt")
 new_file = file.read().split()
 def revword(word:str):
     word = word.lower()[::-1]
@@ -7,11 +7,9 @@ def revword(word:str):
 def countword():
     counter = 1
     word = new_file[0]
-    print(word)
     for x in new_file[1:]:
         temp = revword(x)
         if temp == word:
-            print(temp)
             counter += 1
     return counter
 
